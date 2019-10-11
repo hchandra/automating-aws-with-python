679b800487d9cb7a538b5264d4aaed73cc7b6cdd
Hiren Chandratre <hiren.chandratre@gmail.com>
2019-10-10 22:30:41 -0400

    output website link when syncing

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -5,6 +5,7 @@
 import mimetypes
 from pathlib import Path
 from botocore.exceptions import ClientError
+import util
 
 
 class BucketManager:
@@ -14,6 +15,19 @@ class BucketManager:
         """Create a Bucketmanager object."""
         self.session = session
         self.s3 = self.session.resource('s3')
+
+    def get_region_name(self, bucket):
+        """Get the bucket's region name."""
+        bucket_location = self.s3.meta.client.get_bucket_location(
+                Bucket=bucket.name)
+
+        return bucket_location["LocationConstraint"] or 'us-east-1'
+
+    def get_bucket_url(self, bucket):
+        """Get the website URL for this bucket."""
+        return "http://{}.{}".format(
+                bucket.name,
+                util.get_endpoint(self.get_region_name(bucket)).host)
 
     def all_buckets(self):
         """Get an iterator for all buckets."""
